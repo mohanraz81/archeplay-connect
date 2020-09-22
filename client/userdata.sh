0f1fe4f84fae75e8156fae379e056f460b047098
EC2 Default User <ec2-user@ip-172-31-80-237.ec2.internal>
2020-09-22 04:30:43 +0000

    updated userdata

--- client/userdata.sh
+++ client/userdata.sh
@@ -58,4 +58,34 @@ then
   sed -i "s/EMAILID/$EMAILID/g" addtoconnect.sh
   sed -i "s/PASSWORD/$Password/g" addtoconnect.sh
   sh -x addtoconnect.sh
+  
+  elif [ $os == "AmazonLinux2" ]
+then
+  yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum-config-manager --enable epel
+  yum install bzip2 -y
+  yum install wget -y
+  yum -y groupinstall -y "Xfce"
+  yum install -y xrdp
+  systemctl enable xrdp
+  systemctl start xrdp
+  echo "xfce4-session" > ~ec2-user/.Xclients
+  chmod a+x ~ec2-user/.Xclients
+  sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+  systemctl restart sshd
+  echo ec2-user:$Password | sudo chpasswd
+  cd /usr/local
+  wget http://ftp.mozilla.org/pub/firefox/releases/76.0/linux-x86_64/en-US/firefox-76.0.tar.bz2
+  tar -xvf firefox-76.0.tar.bz2
+  ln -s /usr/local/firefox/firefox /usr/bin/firefox
+  USERNAME="ec2-user"
+  PRIVATEIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+  INSTANCEID=`curl http://169.254.169.254/latest/meta-data/instance-id` 
+  sed -i "s/CONNECTPRIVATEIP/$CONNECTPRIVATEIP/g" addtoconnect.sh
+  sed -i "s/USERNAME/$USERNAME/g" addtoconnect.sh
+  sed -i "s/PRIVATEIP/$PRIVATEIP/g" addtoconnect.sh
+  sed -i "s/INSTANCEID/$INSTANCEID/g" addtoconnect.sh
+  sed -i "s/EMAILID/$EMAILID/g" addtoconnect.sh
+  sed -i "s/PASSWORD/$Password/g" addtoconnect.sh
+  sh -x addtoconnect.sh
 fi
