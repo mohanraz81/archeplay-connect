5266b01283f75a07a6e2a10a112aa0f2f6800880
mounika-at-452647246054 <mounika@archeplay.com>
2020-10-08 11:34:59 +0000

    added connection type ssh input

--- client-ssh/addtoconnect.sh
+++ client-ssh/addtoconnect.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -i -H "Content-Type: application/json" -X POST -d '{"add":[{"EmailId": "EMAILID","InstanceData": [{"InstanceId": "INSTANCEID","UserName": "USERNAME","Password": "PASSWORD" ,"Port": "3389","DnsName": "PRIVATEIP"}]}]}' http://CONNECTPRIVATEIP:9000/api/v1.0/addinstance
+curl -i -H "Content-Type: application/json" -X POST -d '{"add":[{"EmailId": "EMAILID","InstanceData": [{"InstanceId": "INSTANCEID","UserName": "USERNAME","Password": "PASSWORD" ,"Port": "3389","DnsName": "PRIVATEIP","ConnectionType": "ssh"}]}]}' http://CONNECTPRIVATEIP:9000/api/v1.0/addinstance
